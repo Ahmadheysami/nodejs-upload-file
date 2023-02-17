c6ea20ceb18ae16e328d3caa5b4d7304b9a0c942
AhmadHeysami <alheythami@gmail.com>
2023-02-17 16:22:19 +0330

    Preparing the initial files and running the application

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const { Router } = require('express');
 
 const router = new Router()
 const indexController = require('../controllers/indexController');
+
+// @route "/"
+// @method GET
 router.get("/", indexController.getIndex)
 
 module.exports = router
